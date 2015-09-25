97767c17342f56dc13da653f713b2eb4c058a3af
David Gröger <s0544315@htw-berlin.de>
2015-09-25 07:25:06 +0200

    finished bingo book and assassins cloak

--- game/dota_addons/nwu/scripts/vscripts/items/bingo_book.lua
+++ game/dota_addons/nwu/scripts/vscripts/items/bingo_book.lua
@@ -2,8 +2,7 @@ function pingEnemies(keys)
 	local caster = keys.caster
 	local aura_radius = keys.ability:GetSpecialValueFor("aura_radius")
  
-	local targetEntities = FindUnitsInRadius(caster:GetTeamNumber(), caster:GetAbsOrigin(), nil, aura_radius, DOTA_UNIT_TARGET_TEAM_BOTH, DOTA_UNIT_TARGET_BASIC, DOTA_UNIT_TARGET_FLAG_NONE, FIND_ANY_ORDER, false)
-
+	local targetEntities = FindUnitsInRadius(caster:GetTeamNumber(), caster:GetAbsOrigin(), nil, aura_radius, DOTA_UNIT_TARGET_TEAM_ENEMY, DOTA_UNIT_TARGET_HERO, DOTA_UNIT_TARGET_FLAG_NONE, FIND_ANY_ORDER, false)
   
 	ping_duration = keys.ability:GetSpecialValueFor("ping_duration")
 
@@ -12,12 +11,7 @@ if targetEntities then
 		local origin = target:GetAbsOrigin()
 		xcoord = origin.x
 		ycoord = origin.y
-		print(ycoord)
-		print(xcoord)
-		print(target:GetTeamNumber())
-		print(DOTA_MINIMAP_EVENT_HINT_LOCATION)
-		print(target)
-		MinimapEvent(target:GetTeamNumber(), target, xcoord, ycoord, DOTA_MINIMAP_EVENT_HINT_LOCATION, ping_duration)
+		MinimapEvent(caster:GetTeamNumber(), target, xcoord, ycoord, DOTA_MINIMAP_EVENT_HINT_LOCATION, ping_duration)
 	end
 end
 end
