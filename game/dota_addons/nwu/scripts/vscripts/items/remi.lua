94ed138f989a5d268b970930aa133a0428af1b04
David Gröger <s0544315@htw-berlin.de>
2015-10-02 16:13:46 +0200

    fixed shin guards and remi

--- game/dota_addons/nwu/scripts/vscripts/items/remi.lua
+++ game/dota_addons/nwu/scripts/vscripts/items/remi.lua
@@ -4,11 +4,11 @@
 	Called when a Shin Guards is acquired.  Grants the melee block modifier if the caster is a melee hero and the
 	range block modifer if the caster is a range hero.
 ================================================================================================================= ]]
-function modifier_item_shin_guards_datadriven_on_created(keys)
+function modifier_item_remi_datadriven_on_created(keys)
 	if not keys.caster:IsRangedAttacker() then
-		keys.ability:ApplyDataDrivenModifier(keys.caster, keys.caster, "modifier_item_shin_guards_block", {duration = -1})
+		keys.ability:ApplyDataDrivenModifier(keys.caster, keys.caster, "modifier_item_remi_block", {duration = -1})
 	else 
-		keys.ability:ApplyDataDrivenModifier(keys.caster, keys.caster, "modifier_item_shin_guards_block_range", {duration = -1})
+		keys.ability:ApplyDataDrivenModifier(keys.caster, keys.caster, "modifier_item_remi_block_range", {duration = -1})
 	end
 end
 
@@ -18,11 +18,11 @@ end
 	Date: October, 10th 2015
 	Called when a Shin guards is removed from the caster's inventory.  Removes the block modifier.
 ================================================================================================================= ]]
-function modifier_item_shin_guards_datadriven_on_destroy(keys)
+function modifier_item_remi_datadriven_on_destroy(keys)
 	if not keys.caster:IsRangedAttacker() then
-		keys.caster:RemoveModifierByName("modifier_item_shin_guards_block")
+		keys.caster:RemoveModifierByName("modifier_item_remi_block")
 	else 
-		keys.caster:RemoveModifierByName("modifier_item_shin_guards_block_range")
+		keys.caster:RemoveModifierByName("modifier_item_remi_block_range")
 	end
 end
 
@@ -33,30 +33,30 @@ end
 	Called regularly while the caster has a Battle Fury in their inventory.  If the caster has switched from ranged
 	to melee, give them cleave modifier(s).
 ================================================================================================================= ]]
-function modifier_item_shin_guards_datadriven_on_interval_think(keys)
-	if not keys.caster:IsRangedAttacker() and not keys.caster:HasModifier("modifier_item_shin_guards_block") then
+function modifier_item_remi_datadriven_on_interval_think(keys)
+	if not keys.caster:IsRangedAttacker() and not keys.caster:HasModifier("modifier_item_remi_block") then
 		for i=0, 5, 1 do
 			local current_item = keys.caster:GetItemInSlot(i)
 			if current_item ~= nil then
-				if current_item:GetName() == "item_shin_guards" then
-					keys.ability:ApplyDataDrivenModifier(keys.caster, keys.caster, "modifier_item_shin_guards_block", {duration = -1})
+				if current_item:GetName() == "item_remi" then
+					keys.ability:ApplyDataDrivenModifier(keys.caster, keys.caster, "modifier_item_remi_block", {duration = -1})
 				end
-				if keys.caster:HasModifier("modifier_item_shin_guards_block_range") then
-					keys.caster:RemoveModifierByName("modifier_item_shin_guards_block_range")
+				if keys.caster:HasModifier("modifier_item_remi_block_range") then
+					keys.caster:RemoveModifierByName("modifier_item_remi_block_range")
 				end
 			end
 		end
 	end
 
-	if keys.caster:IsRangedAttacker() and not keys.caster:HasModifier("modifier_item_shin_guards_block_range") then
+	if keys.caster:IsRangedAttacker() and not keys.caster:HasModifier("modifier_item_remi_block_range") then
 		for i=0, 5, 1 do
 			local current_item = keys.caster:GetItemInSlot(i)
 			if current_item ~= nil then
-				if current_item:GetName() == "item_shin_guards" then
-					keys.ability:ApplyDataDrivenModifier(keys.caster, keys.caster, "modifier_item_shin_guards_block_range", {duration = -1})
+				if current_item:GetName() == "item_remi" then
+					keys.ability:ApplyDataDrivenModifier(keys.caster, keys.caster, "modifier_item_remi_block_range", {duration = -1})
 				end
-				if keys.caster:HasModifier("modifier_item_shin_guards_block") then
-					keys.caster:RemoveModifierByName("modifier_item_shin_guards_block")
+				if keys.caster:HasModifier("modifier_item_remi_block") then
+					keys.caster:RemoveModifierByName("modifier_item_remi_block")
 				end
 			end
 		end
@@ -71,14 +71,14 @@ end
 	Called regularly while the caster has at least one cleave modifier from Battle Fury.  If the caster is no longer
 	melee (which would be the case on, for example, Troll Warlord), remove the cleave modifiers from the caster.
 ================================================================================================================= ]]
-function modifier_item_shin_guards_block_on_interval_think(keys)
+function modifier_item_remi_block_on_interval_think(keys)
 	if keys.caster:IsRangedAttacker() then
-		while keys.caster:HasModifier("modifier_item_shin_guards_block") do
-			keys.caster:RemoveModifierByName("modifier_item_shin_guards_block")
+		while keys.caster:HasModifier("modifier_item_remi_block") do
+			keys.caster:RemoveModifierByName("modifier_item_remi_block")
 		end
 	else 
-		while keys.caster:HasModifier("modifier_item_shin_guards_block_range") do
-			keys.caster:RemoveModifierByName("modifier_item_shin_guards_block_range")
+		while keys.caster:HasModifier("modifier_item_remi_block_range") do
+			keys.caster:RemoveModifierByName("modifier_item_remi_block_range")
 		end
 	end
 end
