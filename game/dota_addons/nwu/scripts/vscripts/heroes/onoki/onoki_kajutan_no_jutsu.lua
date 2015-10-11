de8cf15fe2639a247beea05d18cfe6abd34d6895
Nicolás Gómez <neeco.gmz@gmail.com>
2015-10-11 03:42:36 -0300

    Onoki [R] mechanics done

---
+++ game/dota_addons/nwu/scripts/vscripts/heroes/onoki/onoki_kajutan_no_jutsu.lua
@@ -0,0 +1,17 @@
+function do_damage(event)
+	local ability = event.ability
+	local caster = event.caster
+	local target = event.target
+	local max_life = target:GetMaxHealth() 
+	local threshold = max_life * ability:GetLevelSpecialValueFor("threshold_factor", ability:GetLevel()-1)
+	local current_life = target:GetHealth()
+	local damage = max_life -- assume the target is below threshold
+	local damage_type = ability:GetAbilityDamageType()
+
+	if current_life > threshold then
+		damage = current_life * ability:GetLevelSpecialValueFor("damage_factor", ability:GetLevel() - 1)
+	end
+
+	--ParticleManager:CreateParticle("particles/blood_impact/blood_advisor_pierce_spray.vpcf", PATTACH_POINT, target)
+	ApplyDamage({ victim = target, attacker = caster, damage = damage, damage_type = damage_type })
+end
