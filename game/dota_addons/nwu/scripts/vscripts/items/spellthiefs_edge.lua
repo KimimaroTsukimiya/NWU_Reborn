cc942624c91a328a0387d05f2d810ab8627cd9f6
David Gröger <s0544315@htw-berlin.de>
2015-10-20 14:12:35 +0200

    imported all first 12 desired heroes from nwu(just model+texture)

--- game/dota_addons/nwu/scripts/vscripts/items/spellthiefs_edge.lua
+++ game/dota_addons/nwu/scripts/vscripts/items/spellthiefs_edge.lua
@@ -2,8 +2,8 @@ require('utilities')
 --[[ ============================================================================================================
 	Author: Dave
 	Date: October 15, 2015
-	Gives the caster gold if he attacks a enemy hero. shortCD = applies of passive is ued, longCD applies on killed creep
-	longCD is changed on the even listener OnEntityKilled->gamemode.lua
+	Gives the caster gold if he attacks a enemy hero. shortCD = applies of passive is used, longCD applies on killed creep
+	longCD is changed on the even listener OnEntityKilled->events.lua
 ================================================================================================================= ]]
 function givegold(keys)
 	local target = keys.target
@@ -23,27 +23,8 @@ function givegold(keys)
 			keys.ability.current_stacks = keys.ability.current_stacks - 1
 			--show the new stack to the player
 			caster:SetModifierStackCount( modifier_name, ability, keys.ability.current_stacks)
-			--if its not on cd do something
-			if not keys.ability.shortCD then
-			--set shordCD true so on reattack it wont trigger again
-			keys.ability.shortCD = true
-			--show the player the cooldown
-			keys.ability:StartCooldown(gold_gain_cd)
-			--start a timer once its reached do something
-			Timers:CreateTimer( gold_gain_cd, function()
-						--if the current stack didnt reach its max limit repeat the former steps (didnt know how to include recursion with timers)
-						if keys.ability.current_stacks < max_stacks then
-							keys.ability.shortCD = false
-							keys.ability.current_stacks = keys.ability.current_stacks + 1
-							keys.ability:StartCooldown(gold_gain_cd)
-							caster:SetModifierStackCount( modifier_name, ability, keys.ability.current_stacks)
-							-- calls a function which basicly does the same until max stacks are reached. 
-							resetCDFirst(keys, modifier_name)
-						end
-						return nil
-			end
-			)
-		end
+			--call stackItem-> starts timers to restack the item
+			GameMode:StackItem(gold_gain_cd, max_stacks, modifier_name, keys.ability, keys.caster)	
 	end
 end
 --[[ ============================================================================================================
@@ -59,55 +40,6 @@ function initgivegold( keys )
 	keys.ability.extra_gold_total = 0
 	keys.caster:SetModifierStackCount( keys.modifier_name, keys.ability, 3)
 	keys.caster:SetModifierStackCount( keys.modifier_name_extra, keys.ability, 0)
-end
---[[ ============================================================================================================
-	Author: Dave
-	Date: October 15, 2015
-	I wasn't sure how recursion works with Timers, therefor i made 2 dirty functions. TODO CLEANER CODE
-	Does check if the support item has less than 3 stacks, if so starts a timer which will add a stack after x seconds cd.
-================================================================================================================= ]]
-function resetCDFirst(keys, modifier_name)
-	local max_stacks = keys.ability:GetLevelSpecialValueFor("gain_gold_max_stacks", (keys.ability:GetLevel() - 1))
-	local gold_gain_cd = keys.ability:GetLevelSpecialValueFor("gold_gain_cd", (keys.ability:GetLevel() - 1))
-	if not keys.ability.shortCD then
-			keys.ability.shortCD = true
-			keys.ability:StartCooldown(gold_gain_cd)
-			Timers:CreateTimer( gold_gain_cd, function()
-						if keys.ability.current_stacks < max_stacks  then
-							keys.ability.shortCD = false
-							keys.ability.current_stacks = keys.ability.current_stacks + 1
-							keys.caster:SetModifierStackCount( modifier_name, keys.ability, keys.ability.current_stacks)
-							resetCDSecond(keys, modifier_name)
-						end
-						return nil
-				end
-			)
-	end
-end
---[[ ============================================================================================================
-	Author: Dave
-	Date: October 15, 2015
-	I wasn't sure how recursion works with Timers, therefor i made 2 dirty functions. TODO CLEANER CODE
-	Does check if the support item has less than 3 stacks, if so starts a timer which will add a stack after x seconds cd.
-================================================================================================================= ]]
-function resetCDSecond(keys, modifier_name)
-	local max_stacks = keys.ability:GetLevelSpecialValueFor("gain_gold_max_stacks", (keys.ability:GetLevel() - 1))
-	local gold_gain_cd = keys.ability:GetLevelSpecialValueFor("gold_gain_cd", (keys.ability:GetLevel() - 1))
-
-	if not keys.ability.shortCD then
-			keys.ability.shortCD = true
-			keys.ability:StartCooldown(gold_gain_cd)
-			Timers:CreateTimer( gold_gain_cd, function()
-						if keys.ability.current_stacks < max_stacks then
-							keys.ability.shortCD = false
-
-							keys.ability.current_stacks = keys.ability.current_stacks + 1
-							keys.caster:SetModifierStackCount( modifier_name, keys.ability, keys.ability.current_stacks)
-						end
-						return nil
-				end
-			)
-	end
 end
 --[[Author: LearningDave
   Date: october, 19th 2015.
@@ -191,18 +123,17 @@ function shareLasthit(keys)
 		  local targetEntities = FindUnitsInRadius(caster:GetTeamNumber(), caster:GetAbsOrigin(), nil, gold_gain_aoe, DOTA_UNIT_TARGET_TEAM_FRIENDLY, DOTA_UNIT_TARGET_HERO, DOTA_UNIT_TARGET_FLAG_NONE, FIND_ANY_ORDER, false)
 		  if targetEntities then
 		    for _,target in pairs(targetEntities) do
-		      if not gold_shared then
+		      if not gold_shared and target ~= caster then
 		      		--TODO find out how to access random gold amount of creep
-		           -- add gold to killerEntity ally hero
 		           target:ModifyGold(45, false, 0)
 		           -- make player see his bonus gold
 		           PopupGoldGain(target, 45)
 		           caster:ModifyGold(45, false, 0)
 		           -- make player see his bonus gold
 		           PopupGoldGain(caster, 45)
-		          keys.ability.current_stacks = keys.ability.current_stacks - 1
-		          caster:SetModifierStackCount( modifier_name, ability, keys.ability.current_stacks)
-		          gold_shared = true
+		           keys.ability.current_stacks = keys.ability.current_stacks - 1
+		           caster:SetModifierStackCount( modifier_name, ability, keys.ability.current_stacks)
+		           gold_shared = true
 		         	GameMode:StackItem(gold_gain_cd, max_stacks, modifier_name, keys.ability, keys.caster)
 		         
 		        end
