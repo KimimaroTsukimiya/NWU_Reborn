1d4de4bf0f8525b49c5f46841313de68ac04f41b
David Gröger <s0544315@htw-berlin.de>
2015-10-30 04:16:10 +0100

    added new cheats: wtf(no cd, retype to disable), gold x(gives player x gold or 500 if no x)

--- game/dota_addons/nwu/scripts/vscripts/cheats.lua
+++ game/dota_addons/nwu/scripts/vscripts/cheats.lua
@@ -1,4 +1,8 @@
+WTF_MODE = false
+
 CHEAT_CODES = {
+    ["wtf"] = function() GameMode:Wtf() end,              -- "Set time of day to dusk"    
+    ["gold"] = function(arg) GameMode:Gold(arg) end,              -- "Set time of day to dusk"    
     ["lvlup"] = function(arg) GameMode:LvlUp(arg) end,                -- "Levels up your hero level x times"        
     ["riseandshine"] = function() GameMode:RiseAndShine() end,        -- "Set time of day to dawn" 
     ["lightsout"] = function() GameMode:LightsOut() end,              -- "Set time of day to dusk"          
@@ -40,11 +44,62 @@ function GameMode:LvlUp(value)
     end
     GameRules:SendCustomMessage("Cheat enabled!", 0, 0)
 end
-
+--[[Author: LearningDave
+  Date: october, 30th 2015.
+  Gives the Hero of the Player who typed 'lvlup x' x level ups
+]]
 function GameMode:RiseAndShine()
     GameRules:SetTimeOfDay( 0.3 )
 end
-
+--[[Author: https://github.com/MNoya/DotaCraft/blob/01a29892b124f695cadd0a134afb8d056c83015a/game/dota_addons/dotacraft/scripts/vscripts/developer.lua
+    Brings the light out!->Daytime
+]]
 function GameMode:LightsOut()
     GameRules:SetTimeOfDay( 0.8 )
+end
+--[[Author: https://github.com/MNoya/DotaCraft/blob/01a29892b124f695cadd0a134afb8d056c83015a/game/dota_addons/dotacraft/scripts/vscripts/developer.lua
+    Night is arriving!->Nighttime
+]]
+function GameMode:Wtf()
+    if WTF_MODE then
+        WTF_MODE = false
+    else
+        WTF_MODE = true
+    end
+    local cmdPlayer = Convars:GetCommandClient()
+    local pID = cmdPlayer:GetPlayerID()
+    local hero = PlayerResource:GetPlayer(pID):GetAssignedHero()
+    if  WTF_MODE then
+        Timers:CreateTimer( function()
+            for i=0, hero:GetAbilityCount()-1 do 
+                if  hero:GetAbilityByIndex(i) ~= nil then
+                    print(i)
+                    hero:GetAbilityByIndex(i):EndCooldown()
+                end
+            end
+            if WTF_MODE then
+                return 0.3
+            else
+                return nil
+            end        
+        end
+        )   
+    end
+    if WTF_MODE then
+        GameRules:SendCustomMessage("Cheat enabled!", 0, 0)
+    else
+        GameRules:SendCustomMessage("Cheat disabled!", 0, 0)
+    end
+end
+--[[Author: LearningDave
+  Date: october, 30th 2015.
+  Gives the Player x Gold (500 if no value given)
+]]
+function GameMode:Gold(value)
+    local cmdPlayer = Convars:GetCommandClient()
+    local pID = cmdPlayer:GetPlayerID()
+    if not value then value = 500 end
+    PlayerResource:ModifyGold(pID, tonumber(value), true, 0)
+
+    GameRules:SendCustomMessage("Cheat enabled!", 0, 0)
 end
