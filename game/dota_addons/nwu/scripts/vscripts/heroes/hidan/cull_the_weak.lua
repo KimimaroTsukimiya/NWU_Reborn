20206124b188d0baee06a89bfd8b9dc5bf7203ef
David Gröger <s0544315@htw-berlin.de>
2015-11-01 22:44:44 +0100

    finished Hidan's Q mechanics

--- game/dota_addons/nwu/scripts/vscripts/heroes/hidan/cull_the_weak.lua
+++ game/dota_addons/nwu/scripts/vscripts/heroes/hidan/cull_the_weak.lua
@@ -3,32 +3,75 @@ function initiateValues( keys )
 end
 
 function gatherTargets( keys )
-	print("TARGETHIT")
-	print("test39")
-	
+	table.insert(keys.ability.hitTargets, keys.target)
 end
 
-function cull_the_weak( keys )
-	print("test222")
+function health_cost( keys )
 	local caster = keys.caster
 	local ability = keys.ability
-	
-	print("test1")
-	print(keys.ability.hitTargets[0])
-	for k,v in ipairs(keys.ability.hitTargets) do
-      print(v.GetAbsOrigin())
-      print("test")
-    end
+	local hp_percentage_cost = ability:GetLevelSpecialValueFor( "hp_percentage_cost", ( ability:GetLevel() - 1 ) )
+	local reduce_hp = caster:GetMaxHealth() / 100 * hp_percentage_cost
+	local new_health = caster:GetHealth() - reduce_hp
+	caster:SetHealth(new_health)
+	PopupDamage(caster, reduce_hp)
+end
+
+
+
+function cull_the_weak( keys )
+	local duration = keys.ability:GetDuration()
+	local caster = keys.caster
+	local ability = keys.ability
+	local creep_damage = ability:GetLevelSpecialValueFor( "creep_damage", ( ability:GetLevel() - 1 ) )
+	local hero_damage = ability:GetLevelSpecialValueFor( "hero_damage", ( ability:GetLevel() - 1 ) )
+
+	for key,oneTarget in pairs(keys.ability.hitTargets) do 
+		print(key,oneTarget) 
+		keys.ability:ApplyDataDrivenModifier(keys.caster, oneTarget, keys.move_slow_modifier, {Duration = duration})
+
+		local vCaster = keys.caster:GetAbsOrigin()
+		local vTarget = oneTarget:GetAbsOrigin()
+		local len = -1 * (( vTarget - vCaster ):Length2D()) + 150
+		local damage = 0
+		if oneTarget:IsHero() then
+			damage = hero_damage
+		else 
+			damage = creep_damage
+		end
+		local knockbackModifierTable =
+		{
+			should_stun = 0,
+			knockback_duration = 0.3,
+			duration = 0.3,
+			knockback_distance = len,
+			knockback_height = 0,
+			center_x = keys.caster:GetAbsOrigin().x,
+			center_y = keys.caster:GetAbsOrigin().y,
+			center_z = keys.caster:GetAbsOrigin().z
+		}
+		oneTarget:AddNewModifier( keys.caster, nil, "modifier_knockback", knockbackModifierTable )
+
+		local damageTable = {
+				victim = oneTarget,
+				attacker = caster,
+				damage = damage,
+				damage_type = ability:GetAbilityDamageType()
+			}
+		ApplyDamage( damageTable )
+
+
+	end
+
 end
 
 
 function release_pull( keys )
 	local target_point = keys.target_points[1]
 	local caster_location = keys.caster:GetAbsOrigin()
-	local range = 400
-	local pull_radius_start = 1000
-	local pull_radius_end = 1000
-	local pull_speed = 1000
+	local range = keys.ability:GetLevelSpecialValueFor( "range", keys.ability:GetLevel() - 1)
+	local pull_radius_start = keys.ability:GetLevelSpecialValueFor( "pull_radius_start", keys.ability:GetLevel() - 1)
+	local pull_radius_end = keys.ability:GetLevelSpecialValueFor( "pull_radius_end", keys.ability:GetLevel() - 1)
+	local pull_speed = 4000
 	local ability = keys.ability
 	local point_difference_normalized = (target_point - caster_location):Normalized()
 	local velocity = point_difference_normalized * pull_speed
@@ -37,7 +80,7 @@ function release_pull( keys )
 	local info = 
 	{
 			Ability = keys.ability,
-        	EffectName = keys.fire_particle,
+
         	vSpawnOrigin = keys.caster:GetAbsOrigin(),
         	fDistance = range,
         	fStartRadius = pull_radius_start,
