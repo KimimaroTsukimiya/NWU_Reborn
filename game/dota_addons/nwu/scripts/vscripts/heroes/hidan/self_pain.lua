1b13fdf33630c4534b99ef622708a9c933ad29f3
David Gröger <s0544315@htw-berlin.de>
2015-11-02 18:07:45 +0100

    finished hidan's T mechanics(self pain)

--- game/dota_addons/nwu/scripts/vscripts/heroes/hidan/self_pain.lua
+++ game/dota_addons/nwu/scripts/vscripts/heroes/hidan/self_pain.lua
@@ -1,3 +1,8 @@
+--[[
+  Author: LearningDave
+  Date: November, 2nd 2015
+  Applies True damage to the caster. Cant kil the caster(1hp will be set)
+]]
 function self_pain( keys )
 	local caster = keys.caster
 	local ability = keys.ability
