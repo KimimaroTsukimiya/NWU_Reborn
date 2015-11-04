f20d20951d5917efafe916799f36573667ee60ee
David Gröger <s0544315@htw-berlin.de>
2015-11-04 03:53:03 +0100

    added gaara's attack attachments

---
+++ game/dota_addons/nwu/scripts/vscripts/heroes/gaara/gaara_ryuusa_bakuryu.lua
@@ -0,0 +1,23 @@
+--[[Author: LearningDave
+	Date: 04.11.2015
+	Creates a dummy at the target location that acts as the Sandstorm
+]]
+function sandstorm( keys )
+	-- Variables
+	local caster = keys.caster
+	local ability = keys.ability
+	local target_point = keys.target_points[1]
+
+	-- Special Variables
+	local duration = ability:GetLevelSpecialValueFor("stun_duration", (ability:GetLevel() - 1))
+
+	-- Dummy
+	local dummy_modifier = keys.dummy_aura
+	local dummy = CreateUnitByName("npc_dummy_unit", target_point, false, caster, caster, caster:GetTeam())
+	dummy:AddNewModifier(caster, nil, "modifier_phased", {})
+	ability:ApplyDataDrivenModifier(caster, dummy, dummy_modifier, {duration = duration})
+
+
+	-- Timer to remove the dummy
+	Timers:CreateTimer(duration, function() dummy:RemoveSelf() end)
+end
