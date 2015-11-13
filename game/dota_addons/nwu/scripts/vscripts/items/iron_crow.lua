ee947c456119019ce3799f442d4f7bf31e35a413
David Groeger <s0544315@htw-berlin.de>
2015-11-13 01:30:25 +0100

    fixed iron crow, now has 2 seconds cd and wont stun structures

---
+++ game/dota_addons/nwu/scripts/vscripts/items/iron_crow.lua
@@ -0,0 +1,6 @@
+function iron_crow( keys )
+	if not keys.target:IsBuilding() and keys.ability:IsCooldownReady() then
+		keys.ability:ApplyDataDrivenModifier(keys.caster, keys.target, keys.modifier_name, {})
+		keys.ability:StartCooldown(keys.ability:GetCooldown(keys.ability:GetLevel()))
+	end
+end
