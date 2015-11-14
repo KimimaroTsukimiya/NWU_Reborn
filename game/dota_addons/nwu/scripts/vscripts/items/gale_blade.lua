a294a290b8c5661b70bed4644bf5ba9c6ba64dfc
David Groeger <s0544315@htw-berlin.de>
2015-11-14 19:31:45 +0100

    kurosawa no longer crits buildings

--- game/dota_addons/nwu/scripts/vscripts/items/gale_blade.lua
+++ game/dota_addons/nwu/scripts/vscripts/items/gale_blade.lua
@@ -1,5 +1,5 @@
  function gale_blade( keys )
  	if not keys.target:IsBuilding() then
- 		keys.ability:ApplyDataDrivenModifier(keys.caster, keys.target, keys.modifier_name, {})
+ 		keys.ability:ApplyDataDrivenModifier(keys.caster, keys.caster, keys.modifier_name, {})
  	end
  end
