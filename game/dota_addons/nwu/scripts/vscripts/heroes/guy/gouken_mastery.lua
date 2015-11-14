0d0b4b56423b921da91a5c20f001a8112478686c
David Groeger <s0544315@htw-berlin.de>
2015-11-14 19:36:08 +0100

    guy's gouken mastery no longer crits buildings

--- game/dota_addons/nwu/scripts/vscripts/heroes/guy/gouken_mastery.lua
+++ game/dota_addons/nwu/scripts/vscripts/heroes/guy/gouken_mastery.lua
@@ -4,18 +4,19 @@
 	Does apply and popup damage to the target (doubled damage of the current attack damage)
 ]]
 function ApplyDoubleDamage( keys )
-	local target = keys.target
-	local caster = keys.caster
-	local ability = keys.ability
-	local abilityDamageType = ability:GetAbilityDamageType()
-	local damage = keys.caster:GetAverageTrueAttackDamage() 
-	PopupDamage(target, damage * 2)
-	local damageTable = {
-			victim = target,
-			attacker = caster,
-			damage = damage,
-			damage_type = abilityDamageType
-		}
-	ApplyDamage( damageTable )
-
+	if not keys.target:IsBuilding() then
+		local target = keys.target
+		local caster = keys.caster
+		local ability = keys.ability
+		local abilityDamageType = ability:GetAbilityDamageType()
+		local damage = keys.caster:GetAverageTrueAttackDamage() 
+		PopupDamage(target, damage * 2)
+		local damageTable = {
+				victim = target,
+				attacker = caster,
+				damage = damage,
+				damage_type = abilityDamageType
+			}
+		ApplyDamage( damageTable )
+	end
 end
