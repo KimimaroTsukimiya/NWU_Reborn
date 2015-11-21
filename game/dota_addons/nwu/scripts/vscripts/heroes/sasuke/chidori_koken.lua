b476f8f404bbba55b140f3d63eadd7aa19869216
Ricky Roller <suzamaki@gmail.com>
2015-11-20 21:06:30 -0700

    initial Sasuke ability work

---
+++ game/dota_addons/nwu/scripts/vscripts/heroes/sasuke/chidori_koken.lua
@@ -0,0 +1,24 @@
+function chidori_koken_check( keys )
+	print('chidoriiiiiii')
+	local caster = keys.caster
+	local ability = keys.ability
+	ListenToGameEvent( "dota_player_used_ability", function( event )
+			local player = GetPlayer( event.PlayerID )
+			-- Check if player existed
+			if player then
+				local hero = player:GetAssignedHero()
+				-- Check if it is corrent hero
+				if hero == caster then
+					-- Check if ability on cast bar is casted
+					local ability_count = caster:GetAbilityCount()
+					for i = 0, (ability_count - 1) do
+						local ability_at_slot = caster:GetAbilityByIndex( i )
+						if ability_at_slot and ability_at_slot:GetAbilityName() == event.abilityname then
+							ability:ApplyDataDrivenModifier( caster, caster, "modifier_overload_damage_datadriven", {} )
+							break
+						end
+					end
+				end
+			end
+	end, nil)
+end
