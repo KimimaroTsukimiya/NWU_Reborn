edc74c8483fcbd2878a4ae14b093d0ed7f7b3e66
David Groeger <s0544315@htw-berlin.de>
2015-11-21 08:58:01 +0100

    fixed iron crow, illusions no longer bash

--- game/dota_addons/nwu/scripts/vscripts/items/iron_crow.lua
+++ game/dota_addons/nwu/scripts/vscripts/items/iron_crow.lua
@@ -1,5 +1,5 @@
 function iron_crow( keys )
-	if not keys.target:IsBuilding() and keys.ability:IsCooldownReady() then
+	if not keys.target:IsBuilding() and keys.ability:IsCooldownReady() and keys.caster:IsRealHero() then
 		keys.ability:ApplyDataDrivenModifier(keys.caster, keys.target, keys.modifier_name, {})
 		keys.ability:StartCooldown(keys.ability:GetCooldown(keys.ability:GetLevel()))
 	end
