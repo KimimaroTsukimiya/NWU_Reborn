fc58020a2f26ebb94a1720394645b9a46b46b295
David Groeger <s0544315@htw-berlin.de>
2015-11-21 09:00:30 +0100

    fixed scroll of seals, illusions have the same hp as the caster on use now

--- game/dota_addons/nwu/scripts/vscripts/items/probagation.lua
+++ game/dota_addons/nwu/scripts/vscripts/items/probagation.lua
@@ -43,6 +43,7 @@ function ConjureImage( event )
   end
  end
 
+ illusion:SetHealth(caster:GetHealth())
 
  -- Set the unit as an illusion
  -- modifier_illusion controls many illusion properties like +Green damage not adding to the unit damage, not being able to cast spells and the team-only blue particle 
