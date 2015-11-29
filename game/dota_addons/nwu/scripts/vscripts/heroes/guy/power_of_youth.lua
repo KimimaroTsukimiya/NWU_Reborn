2a8e9b078c5a902ae76103a1f662bc1503ec8a4c
Mognakor <haroshika@hotmail.de>
2015-11-29 22:17:03 +0100

    guy fix, power of youth not yet tested

--- game/dota_addons/nwu/scripts/vscripts/heroes/guy/power_of_youth.lua
+++ game/dota_addons/nwu/scripts/vscripts/heroes/guy/power_of_youth.lua
@@ -1,44 +1,40 @@
---[[
-  Author: LearningDave
-  Date: October, 28th 2015
-  Initiates the variables which are needed for 'power_of_youth'
-]]
-function power_of_youth_initiate( keys )
-	keys.ability.stacks = 0
-	keys.ability.attack_speed = keys.caster:GetAttackSpeed()
-	keys.ability.same_target = 0
-	keys.ability.new_attack_speed = 0
-end
---[[
-  Author: LearningDave
-  Date: October, 28th 2015
-  Stacks Guy's attackspeed if he is hitting the same target, if not bonus is devicded by 2 (max 100)
-]]
 function power_of_youth( keys )
 	local caster = keys.caster
-	local ability = keys.ability
 	local target = keys.target
-	local attack_speed_bonus_percentage = ability:GetLevelSpecialValueFor( "bonus_attack_speed", ability:GetLevel() - 1)
-	local max_stacks = ability:GetLevelSpecialValueFor( "max_stacks", ability:GetLevel() - 1)
-	local stacks = ability.stacks
-	local attack_speed = keys.ability.attack_speed
-	local new_attack_speed = attack_speed
-	local sametarget = keys.same_target
-	if keys.ability.same_target == target then
-		if keys.ability.stacks < max_stacks then
-			keys.ability.stacks = keys.ability.stacks + 1
-		end
-	else
-		if keys.ability.stacks ~= 0 then	
-				keys.ability.stacks = keys.ability.stacks / 2
-				keys.ability.stacks = math.floor(keys.ability.stacks)
-		else
-			keys.ability.stacks = 1
-		end
-	end	
-	new_attack_speed = keys.ability.stacks * attack_speed_bonus_percentage
-	keys.ability.same_target = target
+	local ability = keys.ability
+	local ability_level = ability:GetLevel() - 1
+	local modifier = keys.modifier
+	local max_stacks = ability:GetLevelSpecialValueFor("max_stacks", ability_level)
 
-	--SetModifierStackCount does MULTIPLY ALL properties of the modifier with the 3rd parameter!!!
-	caster:SetModifierStackCount("modifier_guy_power_of_youth_as_bonus", caster, new_attack_speed)
+	-- Check if we have an old target
+	if caster.POY_target == nil then
+		caster.POY_target = target
+		return
+	end
+	
+	-- Check if the caster has the attack speed modifier
+	if not caster:HasModifier(modifier) then
+		-- Apply the attack speed modifier and set the starting stack number
+		ability:ApplyDataDrivenModifier(caster, caster, modifier, {})
+		caster:SetModifierStackCount(modifier, ability, 1)
+		return
+	end
+	
+	-- Get the current stacks
+	local stack_count = caster:GetModifierStackCount(modifier, ability)
+	
+	-- Check if that old target is the same as the attacked target
+	if caster.POY_target ~= target then
+		-- If its not the same target then set it as the new target and halve the modifier
+		caster:SetModifierStackCount(modifier, ability, stack_count/2 + 1)
+		caster.POY_target = target
+		return
+	end
+	
+	-- Check if the current stacks are lower than the maximum allowed
+	if stack_count < max_stacks then
+		-- Increase the count if they are
+		caster:SetModifierStackCount(modifier, ability, stack_count + 1)
+	end
+	
 end
