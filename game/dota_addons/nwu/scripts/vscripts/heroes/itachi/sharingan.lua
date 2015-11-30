c27577df1a779bcc7c820dc556a8948387fabd83
David Groeger <s0544315@htw-berlin.de>
2015-11-30 13:36:16 +0100

    nerfed itachi's sharingan, passive takes basedamage now

--- game/dota_addons/nwu/scripts/vscripts/heroes/itachi/sharingan.lua
+++ game/dota_addons/nwu/scripts/vscripts/heroes/itachi/sharingan.lua
@@ -3,9 +3,12 @@ function sharingan( keys )
 		local ability    = keys.ability
 		local target     = keys.target
 		local duration 	 = keys.ability:GetLevelSpecialValueFor("duration", keys.ability:GetLevel() - 1)
-		local damage 	 = keys.caster:GetAverageTrueAttackDamage() 
+		local damage 	 = keys.caster:GetBaseDamageMax()
+		print(damage)
+		print(keys.caster:GetBaseDamageMin())
+		print(keys.caster:GetAgility())
+		keys.ability:ApplyDataDrivenModifier(keys.caster, target, "modifier_itachi_sharingan_mr_reduce", {duration = duration})
 		ApplyDamage({victim = target, attacker = keys.caster, damage = damage, damage_type = DAMAGE_TYPE_MAGICAL})
 		PopupDamage(target, damage)
-		keys.ability:ApplyDataDrivenModifier(keys.caster, target, "modifier_itachi_sharingan_mr_reduce", {duration = duration})
 	end
 end
