f1579704c1de5436b29a45dfdcb5f1cf7696dec3
Mognakor <haroshika@hotmail.de>
2015-12-03 21:01:16 +0100

    shika 4/4

---
+++ game/dota_addons/nwu/scripts/vscripts/heroes/shikamaru/kage_nui.lua
@@ -0,0 +1,10 @@
+function SetDamage( event )
+	local target = event.target
+	local ability = event.ability
+	local attack_damage_min = ability:GetLevelSpecialValueFor("damage_min", ability:GetLevel() - 1 )
+	local attack_damage_max = ability:GetLevelSpecialValueFor("damage_max", ability:GetLevel() - 1 )
+
+	target:SetBaseDamageMax(attack_damage_max)
+	target:SetBaseDamageMin(attack_damage_min)
+
+end
