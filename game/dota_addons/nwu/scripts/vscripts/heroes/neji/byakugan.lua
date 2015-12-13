14f3fd57f234ee195bdd1755c1aabe02c2b1c347
David Groeger <s0544315@htw-berlin.de>
2015-12-13 16:06:00 +0100

    fixed neji's bya

--- game/dota_addons/nwu/scripts/vscripts/heroes/neji/byakugan.lua
+++ game/dota_addons/nwu/scripts/vscripts/heroes/neji/byakugan.lua
@@ -8,16 +8,8 @@ function vision( keys )
 	local ability_level = ability:GetLevel() - 1
 	local radius = ability:GetLevelSpecialValueFor("vision_aoe", ability_level)
 	local duration = ability:GetLevelSpecialValueFor("duration", ability_level)
-	local dayTime = caster:GetDayTimeVisionRange()
-	local night = caster:GetNightTimeVisionRange()
-	keys.caster:SetDayTimeVisionRange(radius)
-	keys.caster:SetNightTimeVisionRange(radius)
-	Timers:CreateTimer( duration, function()
-		print(dayTime)
-		print(night)
-    	keys.caster:SetDayTimeVisionRange(dayTime)
-		keys.caster:SetNightTimeVisionRange(night)
-		return nil
-	end
-	)
+
+	AddFOWViewer(caster:GetTeamNumber(), caster:GetAbsOrigin(), radius, duration, false)
+
+
 end
