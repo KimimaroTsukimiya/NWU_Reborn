aac16fa134a09044dca2eded7bcefebd57b86e81
Mognakor <haroshika@hotmail.de>
2015-12-19 19:22:55 +0100

    CA fixes

--- game/dota_addons/nwu/scripts/vscripts/heroes/sasuke/chidori_eiso.lua
+++ game/dota_addons/nwu/scripts/vscripts/heroes/sasuke/chidori_eiso.lua
@@ -1,18 +1,23 @@
+require('items')
+
 function chidori_eiso(keys)
 	local target = keys.target
 	local caster = keys.caster
 	local particle = keys.particle
 	local ability = keys.ability
-	local ability_level = ability:GetLevel() - 1
-	local damage = ability:GetLevelSpecialValueFor("damage", ability_level)
-	local duration = ability:GetLevelSpecialValueFor("stun_duration", ability_level)
+	local duration = ability:GetLevelSpecialValueFor("stun_duration", ability:GetLevel() - 1)
 
 	-- Lightning particle
 	local pid = ParticleManager:CreateParticle(particle, PATTACH_CUSTOMORIGIN, caster)
     ParticleManager:SetParticleControl(pid, 0, target:GetAbsOrigin() + Vector(0, 0, 75))
     ParticleManager:SetParticleControl(pid, 1, caster:GetAbsOrigin() + Vector(0, 0, 75))
 
-    -- Apply Damage and stun
-	ApplyDamage({attacker = caster, victim = target, ability = ability, damage = damage, damage_type = ability:GetAbilityDamageType()})
-	target:AddNewModifier(caster, ability, "modifier_stunned", {duration = duration})
+	local event = keys
+	keys.GenericFunction = "GenericSpellFunction"
+	keys.Modifier1 = "modifier_chidori_eiso"
+	keys.Duration1 = duration
+	keys.doDamage = true
+	
+	CheckForSpellBlock(event)
+	
 end
