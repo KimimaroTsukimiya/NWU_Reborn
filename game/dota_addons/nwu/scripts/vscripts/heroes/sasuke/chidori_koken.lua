e67381cbc4110f16aa2288a11888ed83f0a3429f
David Groeger <s0544315@htw-berlin.de>
2015-12-21 05:18:52 +0100

    fixed sasuke's passive (wont trigger while denying/hitting ally units anymore)

--- game/dota_addons/nwu/scripts/vscripts/heroes/sasuke/chidori_koken.lua
+++ game/dota_addons/nwu/scripts/vscripts/heroes/sasuke/chidori_koken.lua
@@ -21,3 +21,43 @@ function chidori_koken_check( keys )
 			end
 	end, nil)
 end
+
+function releaseAoeDamage (keys)
+
+if keys.target:GetTeamNumber() ~= keys.caster:GetTeamNumber() then
+
+		local caster = keys.caster
+		local aoe_radius = keys.ability:GetLevelSpecialValueFor("aoe_radius", keys.ability:GetLevel() - 1)
+		local damage = keys.ability:GetAbilityDamage()
+		local target = keys.target
+
+		EmitSoundOn("Hero_StormSpirit.Overload", target)
+
+		local particle = ParticleManager:CreateParticle("particles/units/heroes/hero_stormspirit/stormspirit_overload_discharge.vpcf", PATTACH_ABSORIGIN, keys.target) 
+		ParticleManager:SetParticleControlEnt(particle, 0, keys.caster, PATTACH_ABSORIGIN, "attach_hitloc", keys.caster:GetAbsOrigin(), true)
+					
+		
+		caster:RemoveModifierByName("modifier_chidori_koken_damage_datadriven")
+
+
+		local targetEntities = FindUnitsInRadius(caster:GetTeamNumber(), target:GetAbsOrigin(), nil, aoe_radius, DOTA_UNIT_TARGET_TEAM_ENEMY, DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_CREEP, 0, FIND_ANY_ORDER, false)
+		
+		if targetEntities then
+			for _,target in pairs(targetEntities) do
+				print(abilityDamageType)
+				print(damage)
+				print(target)
+				print(caster)
+				local damageTable = {
+						victim = target,
+						attacker = caster,
+						damage = damage,
+						damage_type = DAMAGE_TYPE_MAGICAL
+					}
+				ApplyDamage( damageTable )
+				keys.ability:ApplyDataDrivenModifier(caster, target, "modifier_chidori_koken_debuff_datadriven", {})
+			end
+		end
+
+end
+end
