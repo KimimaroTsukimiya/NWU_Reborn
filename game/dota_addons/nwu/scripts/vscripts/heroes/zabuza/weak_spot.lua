45883c33b870ae1904085ad22d07f24500ac8b00
David Groeger <s0544315@htw-berlin.de>
2015-12-21 07:00:45 +0100

    fixed zabuza's weak spot, attacks will reduce the cooldown by one second

--- game/dota_addons/nwu/scripts/vscripts/heroes/zabuza/weak_spot.lua
+++ game/dota_addons/nwu/scripts/vscripts/heroes/zabuza/weak_spot.lua
@@ -10,13 +10,24 @@ function weak_spot( keys )
 		local cooldown = ability:GetCooldown(level)
 		local caster = keys.caster	
 		local modifierName = "modifier_weak_spot"
-
 		ability:StartCooldown(cooldown)
 
 		caster:RemoveModifierByName(modifierName) 
-
-		Timers:CreateTimer(cooldown, function()
-			ability:ApplyDataDrivenModifier(caster, caster, modifierName, {})
-			end)
 	end	
 end
+
+function weak_spot_reset_cd (keys)
+	if keys.target:GetTeamNumber() ~= keys.caster:GetTeamNumber() and not keys.target:IsBuilding() then		
+		local new_cd = keys.ability:GetCooldownTimeRemaining() - 1.0
+		keys.ability:EndCooldown()
+		keys.ability:StartCooldown(new_cd)
+	end
+end
+
+function addCrit( keys )
+	if keys.ability:IsCooldownReady() then 
+		local modifierName = "modifier_weak_spot"
+		 keys.ability:ApplyDataDrivenModifier(keys.caster, keys.caster, modifierName, {})
+	end
+end
+
