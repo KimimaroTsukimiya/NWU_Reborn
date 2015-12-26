c4c4f8e19613186959ce5958f198edc3aad68e71
David Groeger <s0544315@htw-berlin.de>
2015-12-26 19:03:57 +0100

    fixed temari: ulti dmg+desc, items no longer trigger passive, reduced castime of e from 0,5 to 0,3

--- game/dota_addons/nwu/scripts/vscripts/heroes/temari/temari_sheer_wind.lua
+++ game/dota_addons/nwu/scripts/vscripts/heroes/temari/temari_sheer_wind.lua
@@ -51,3 +51,64 @@ function sheer_wind( keys )
 		end
 	end
 end
+
+function sheer_wind_call( keys )
+		local ability_index = keys.caster:FindAbilityByName("temari_sheer_wind"):GetAbilityIndex()
+    local sheer_wind_ability = keys.caster:GetAbilityByIndex(ability_index)
+
+    if sheer_wind_ability:GetLevel() > 0 then
+
+    	local caster = keys.caster
+		local ability = sheer_wind_ability
+		local maxStack = ability:GetLevelSpecialValueFor("sheer_wind_max_stacks", (ability:GetLevel() - 1))
+		local modifierCount = caster:GetModifierCount()
+		local currentStack = 0
+		local modifierBuffName = "modifier_sheer_wind_buff"
+		local modifierStackName = "modifier_sheer_wind_buff_stack"
+		local modifierName
+
+
+		-- Always remove the stack modifier
+		caster:RemoveModifierByName(modifierStackName) 
+
+		-- Counts the current stacks
+		for i = 0, modifierCount do
+			modifierName = caster:GetModifierNameByIndex(i)
+
+			if modifierName == modifierBuffName then
+				currentStack = currentStack + 1
+			end
+		end
+
+		-- Remove all the old buff modifiers
+		for i = 0, currentStack do
+			caster:RemoveModifierByName(modifierBuffName)
+		end
+
+		-- Always apply the stack modifier 
+		ability:ApplyDataDrivenModifier(caster, caster, modifierStackName, {})
+
+		-- Reapply the maximum number of stacks
+		if currentStack >= maxStack then
+			caster:SetModifierStackCount(modifierStackName, ability, maxStack)
+
+			-- Apply the new refreshed stack
+			for i = 1, maxStack do
+				ability:ApplyDataDrivenModifier(caster, caster, modifierBuffName, {})
+			end
+		else
+			-- Increase the number of stacks
+			currentStack = currentStack + 1
+
+			caster:SetModifierStackCount(modifierStackName, ability, currentStack)
+
+			-- Apply the new increased stack
+			for i = 1, currentStack do
+				ability:ApplyDataDrivenModifier(caster, caster, modifierBuffName, {})
+			end
+		end
+
+
+    end
+
+end
