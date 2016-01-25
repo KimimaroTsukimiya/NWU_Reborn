6c34889bd5a8d519c0267271354e0486c7d0cca4
David Groeger <s0544315@htw-berlin.de>
2016-01-25 02:49:07 +0100

    nerfs itachi's sharingan wont be triggered from his illus anylonger

--- game/dota_addons/nwu/scripts/vscripts/heroes/itachi/sharingan.lua
+++ game/dota_addons/nwu/scripts/vscripts/heroes/itachi/sharingan.lua
@@ -1,5 +1,5 @@
 function sharingan( keys )
-	if not keys.target:IsBuilding() and keys.target:GetTeamNumber() ~= keys.caster:GetTeamNumber() then
+	if not keys.target:IsBuilding() and keys.target:GetTeamNumber() ~= keys.caster:GetTeamNumber() and keys.caster:IsRealHero() then
 		local ability    = keys.ability
 		local target     = keys.target
 		local duration 	 = keys.ability:GetLevelSpecialValueFor("duration", keys.ability:GetLevel() - 1)
